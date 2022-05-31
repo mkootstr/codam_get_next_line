9569ba89b6cfc2d847a8c362e8426abe92e4b70b
Marlou Kootstra <marlou@Marlous-MacBook-Pro.local>
2022-05-31 17:48:06 +0200

    rightfilethistime

--- get_next_line.c
+++ get_next_line.c
@@ -6,79 +6,63 @@
 /*   By: mkootstr <mkootstr@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/21 11:14:17 by mkootstr      #+#    #+#                 */
-/*   Updated: 2021/10/14 14:23:11 by mkootstr      ########   odam.nl         */
+/*   Updated: 2021/10/25 18:10:19 by mkootstr      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 char	*get_next_line(int	fd)
 {
-	static char		buf[BUFFER_SIZE + 1];
-	char			**line;
-	ssize_t			br;
-	char			*lline;
+	static t_buf	buf = (t_buf){.size = 0};
+	char			*line;
+	t_status		status;
 
-	lline = NULL;
-	line = &lline;
-	br = 0;
-	//printf("%d\n", 1);
-	while (buf[0] != '\0' || br == 0)
+	line = NULL;
+	while (1)
 	{
-		br = read(fd, buf, BUFFER_SIZE);
-		if (br == 0)
+		if (!buf.size)
+			buf.size = read(fd, buf.buf, BUFFER_SIZE);
+		buf.buf[buf.size] = '\0';
+		if (buf.size == 0)
+			return (line);
+		else if (buf.size < 0)
 		{
-			//printf("%d\n", 2);
+			if (line)
+				free(line);
 			return (NULL);
 		}
-		else if (br < 0)
-		{
-			//printf("%d\n", 3);
-			if (*line)
-			{
-				free(*line);
-				//printf("%d\n", 4);
-			}
-			return (NULL);
+		else if (buf.size > 0)
+		{	
+			status = ft_prcsbuf(&line, buf.buf, &buf.size);
+			if (status == NEWLINE_FOUND)
+				return (line);
 		}
-		else if (br > 0)
-		{
-			//printf("%d\n", 5);
-			ft_prcsbuf(line, buf, br);
-		}
-		if (buf[0] == '\n' || *line == NULL)
-			break ;
 	}
-	return (*line);
 }
 
-char	*ft_prcsbuf(char **line, char *buf, ssize_t br)
+t_status	ft_prcsbuf(char **line, char *buf, ssize_t *br)
 {
-	ssize_t	n;
-	size_t	i;
-	size_t	nl;
+	ssize_t		n;
+	size_t		i;
+	t_status	status;
 
-	n = br;
+	n = *br;
 	i = 0;
-	nl = 0;
-	//printf("%d\n", 6);
-	while (n > 0 && buf[i] != '\n')
+	while (n > 1 && buf[i] != '\n')
 	{
 		i++;
 		n--;
 	}
-	if (!*line)
-	{
-		*line = ft_strndup(buf, i);
-		//printf("%d\n", 7);
-	}
-	else if (*line)
-	{
-		*line = ft_strfrjoin(*line, buf, i);
-		//printf("%d\n", 8);
-	}
-	ft_memmove(&buf[i + 1], &buf[0], (br - i));
-	//printf("%d\n", 9);
-	return (buf);
+	if (!(*line))
+		*line = ft_strndup(buf, (i + 1));
+	else
+		ft_append(line, buf, (i + 1));
+	if (buf[i] == '\n' || !(*line))
+		status = NEWLINE_FOUND;
+	else
+		status = NEWLINE_NOT_FOUND;
+	ft_memmove(buf, &buf[i + 1], (*br - i));
+	*br -= (i + 1);
+	return (status);
 }
